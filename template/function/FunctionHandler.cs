a57117da96313d60e9635b6f40b832cbafda9655
Burton Rheutan <rheutan7@gmail.com>
2018-02-14 01:24:11 -0600

    initial

---
+++ template/function/FunctionHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Function
+{
+    public class FunctionHandler
+    {
+        public Task<string> Handle(string input)
+        {
+            return Task.FromResult($"Hello! Your input was {input}");
+        }
+    }
+}
