a14c818dade8b8d5995298499f81c422f811ac64
Burton Rheutan <rheutan7@gmail.com>
2019-05-10 23:15:06 -0500

    Add csharp-http version of template
    Created a new version of the template in order to prevent breaking
    existing functions that rely on the previous style of handler
    
    This new version exposes more of the function request to the handler
    by way of the OpenFaaS.FunctionSDK NuGet package types
    
    Signed-off-by: Burton Rheutan <rheutan7@gmail.com>

---
+++ template/csharp-http/Startup.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Function;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Primitives;
+using Newtonsoft.Json;
+using OpenFaaS.FunctionSDK;
+
+namespace root
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                var functionHandler = new FunctionHandler();
+                try
+                {
+                    // extract the request information
+                    var requestBody = getRequest(context);
+                    var requestHeaders = getHeaders(context);
+                    var requestQueries = getQuery(context);
+
+                    // set the context to pass to the function
+                    var fnContext = new FunctionContext
+                    {
+                        Body = requestBody,
+                        Headers = requestHeaders,
+                        Method = new HttpMethod(context.Request.Method),
+                        QueryString = requestQueries
+                    };
+
+                    // execute the function
+                    var result = await functionHandler.Handle(fnContext);
+                    
+                    // set the response from the FunctionResponse
+                    context.Response.ContentType = "application/json";
+                    context.Response.StatusCode = result.StatusCode.ToStatusInt();
+
+                    // if headers were set, convert to the proper type and set the response
+                    if (result.Headers != null && result.Headers.Count > 0)
+                    {
+                        foreach (var head in result.Headers.AllKeys)
+                        {
+                            context.Response.Headers.Add(new KeyValuePair<string, StringValues>(head, result.Headers[head]));
+                        }
+                    }
+
+                    // set the response body. Assume object if it's not a stream
+                    if (result.Body.GetType() == typeof(Stream))
+                    {
+                        context.Response.Body = (Stream)result.Body;
+                    }
+                    else
+                    {
+                        await context.Response.WriteAsync(JsonConvert.SerializeObject(result.Body));
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // if there's an error, return appropriate response code and the exception message
+                    context.Response.StatusCode = 500;
+                    context.Response.ContentType = "application/json";                    
+                    await context.Response.WriteAsync(JsonConvert.SerializeObject(new { Error = ex.Message }));
+                }
+            });
+        }
+
+        private object getRequest(HttpContext context)
+        {
+            StreamReader reader = new StreamReader(context.Request.Body);
+            string sBody = reader.ReadToEnd();
+            object body = sBody;
+
+            // If the request is JSON, convert it to a JObject for easy deserialization in the handler
+            if (sBody[0] == '{')
+            {
+                body = JsonConvert.DeserializeObject(sBody);
+            }
+
+            return body;
+        }
+
+        private NameValueCollection getHeaders(HttpContext context)
+        {
+            var results = new NameValueCollection();
+            var headers = context.Request.Headers;
+            foreach (var h in headers)
+            {
+                results.Add(h.Key, string.Join('|', h.Value));
+            }
+            return results;
+        }
+
+        private NameValueCollection getQuery(HttpContext context)
+        {
+            var queries = new NameValueCollection();
+            var reqQuery = context.Request.Query;
+
+            foreach (var q in reqQuery)
+            {
+                queries.Add(q.Key, string.Join('|', q.Value));
+            }
+            return queries;
+        }
+    }
+}
---
+++ template/csharp-http/function/FunctionHandler.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using OpenFaaS.FunctionSDK;
+
+namespace Function
+{
+    /// <summary>
+    /// This is only here to demonstrate that custom request objects can be used
+    /// Ideally, this type would be included from your specific NuGet package,
+    /// not directly in the function
+    /// </summary>
+    public class SampleRequest
+    {
+        public string Name { get; set; }
+    }
+
+
+    /// <summary>
+    /// Similar to the SampleRequest class above, there is the ability to use
+    /// a custom response object. Again, ideally this would be included from a
+    /// NuGet package and shared with the calling application
+    /// </summary>
+    public class SampleResponse
+    {
+        public string Response { get; set; }
+    }
+
+    public class FunctionHandler
+    {
+        public Task<FunctionResponse> Handle(FunctionContext fnContext)
+        {
+            // Sample: Set custom headers
+            var resultHeaders = new NameValueCollection
+            {
+                {"X-OpenFaaS-Function", "csharp-kestrel"},
+            };
+
+            // Sample: use a custom request type by casting the fnContext.Body to JObject and deserializing it
+            // var jBody = (JObject)fnContext.Body;
+            // SampleRequest req = jBody.ToObject<SampleRequest>();
+            
+            var result = new FunctionResponse {
+                // Sample: Return plain text, or a custom object as the body
+                // Body = $"Hello from OpenFaaS + Kestrel, {fnContext.Body}!",
+                Body = new SampleResponse
+                {
+                    Response = $"Hello from OpenFaaS + Kestrel, {fnContext.Body}"
+                },
+                Headers = resultHeaders
+            };
+
+            return Task.FromResult(result);
+        }
+    }
+}
